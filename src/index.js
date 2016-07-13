64b024ee4e4743d1bcdf69f1ccaee3fabdcb333d
Dave Harms <daveharmswebdev@gmail.com>
2016-07-13 16:52:01 -0500

    added comments

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const Handlebars = require('hbsfy/runtime');
 const special = require('../src/templates/special.hbs');
 var special2 = require('../src/templates/specialSpecial.hbs');
 
+// this is the data
 let cities = ['Nashville', 'San Francisco'];
 
 Handlebars.registerPartial('special', special);
+// this partial has the known helper each block
 Handlebars.registerPartial('specialSpecial', special2);
 Handlebars.registerHelper('bold', function(data, options) {
 	console.log(data);
@@ -21,4 +23,5 @@ Handlebars.registerHelper('bold2', function(data, options) {
 	return new Handlebars.SafeString(result);
 });
 
+// test is the hbs file with the specialSpecial partial
 $('body').append(test({cities: cities, body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero eum ipsa accusantium, id saepe deserunt eligendi quae unde est fuga molestias laborum, sed expedita adipisci repellat dolorum, quidem, fugit iure.', title: 'Dave'}));
